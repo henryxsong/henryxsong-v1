c8f23b5e8148740253ad7c86479c212f8dab6ab0
Henry Song <henry.song671@gmail.com>
2022-01-24 21:38:42 -0800

    added StyledSection in hero.js

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,9 +1,23 @@
 import React from "react"
 import styled from "styled-components"
 
+const StyledSection = styled.section`
+    .title {
+        margin-bottom: 0;
+    }
+    .subtitle {
+        margin-top: 0;
+    }
+    .highlighted {
+        box-shadow: inset 0 -2.5rem 0 #cdf3e1;
+    }
+`
+
 const Hero = () => {
     return (
+        <StyledSection id="hero">
         
+        </StyledSection>
     )
 }
 
