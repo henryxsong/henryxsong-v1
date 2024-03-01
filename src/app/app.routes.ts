eb4fa5c7148420622dd74e7aa39ed85d39666b04
Henry Song <henry.song671@gmail.com>
2024-03-01 11:42:33 -0800

    added main & page not found components to routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { MainComponent } from './main/main.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: MainComponent}, // Default route
+    {path: '**', component: PageNotFoundComponent} // Wildcard route for a 404 page
+];
