d5ef0838756deb34fa2ae3941bb5f3bf7a24c076
Henry Song <henry.song671@gmail.com>
2024-03-02 11:45:01 -0800

    changed socials componant to standalone

--- src/app/socials/socials.component.ts
+++ src/app/socials/socials.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-socials',
-  standalone: false,
+  standalone: true,
+  imports: [],
   templateUrl: './socials.component.html',
   styleUrl: './socials.component.css'
 })
