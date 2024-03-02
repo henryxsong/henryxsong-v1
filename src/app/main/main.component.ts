82cf7478db606689202d9f091d88aa831c7b3981
Henry Song <henry.song671@gmail.com>
2024-03-02 11:37:31 -0800

    changed Main component to non-standalone

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { SocialsComponent } from '../socials/socials.component';
 
 @Component({
   selector: 'app-main',
-  standalone: true,
-  imports: [],
+  standalone: false,
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
