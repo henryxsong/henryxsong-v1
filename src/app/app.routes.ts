22ee66bee9681ce3f008b230e5b4b5874c10d341
Henry Song <henry.song671@gmail.com>
2024-03-02 16:03:39 -0800

    added routes of components in app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,10 @@
 import { Routes } from '@angular/router';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { MainComponent } from './main/main.component';
+import { AboutComponent } from './about/about.component';
+import { EducationComponent } from './education/education.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { SocialsComponent } from './socials/socials.component';
 
 export const routes: Routes = [
     {
@@ -8,6 +12,29 @@ export const routes: Routes = [
         title: 'Henry Song',
         component: MainComponent
     }, // Default route
+
+    {
+        path: '#about', 
+        title: 'About Me',
+        component: AboutComponent
+    }, // about route
+    {
+        path: '#education', 
+        title: 'Education',
+        component: EducationComponent
+    }, // education route
+
+    {
+        path: '#experience', 
+        title: 'Experience',
+        component: ExperienceComponent
+    }, //Experience route
+
+    {
+        path: '#socials', 
+        title: 'Socials',
+        component: SocialsComponent
+    }, // Socials route
 
     {
         path: '**', 
